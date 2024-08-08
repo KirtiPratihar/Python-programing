90f9580c38950dda6429781d7c0a1f04f19b44f8
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2024-08-08 20:52:42 +0530

    Update Number_guessing_game.py

--- Number_guessing_game.py
+++ Number_guessing_game.py
@@ -1,1 +1,21 @@
 #random Number Generator
+import random
+print("WELCOME TO THE NUMBER GUESSING GAME")
+n =random.randrange(1,100)
+attempts=0
+
+while True:
+    try:
+        k=int(input("YOU HAVE TO CHOOSE A NUMBER BETWEEN 1 TO 100-"))
+        attempts=attempts+1
+        if (k>100 or k<0):
+            print("NOT VALID")
+        elif k<n:
+            print("YOU CHOOSED A SMALLER NUMBER")
+        elif k>n:
+            print("YOU HAVE CHOOSE A LARGER NUMBER")
+        else:
+            print(f"YOU HAVE SUCESSFULLY GUESSED THE CORRECT NUMBER WITH {attempts} ATTEMPTS".format(attempts))
+            break
+    except ValueError:
+        print("Please enter a valid Number")
