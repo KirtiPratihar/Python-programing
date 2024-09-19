5e99c49b06e016eb30f2df63254de6d8fa3bf383
Kirti Pratihar <167183967+KirtiPratihar@users.noreply.github.com>
2024-09-19 21:31:41 +0530

    Update pt2.py

--- BASIC/pt2.py
+++ BASIC/pt2.py
@@ -1,9 +1,12 @@
-l=[1,2,3,2,1,3,4,5,5,3,2,4,3,2]
 from collections import Counter
-c=Counter(l)
 
-for key,values in c.items():
-  if value>1:
-    return key
+l = [1, 2, 3, 2, 1, 3, 4, 5, 5, 3, 2, 4, 3, 2]
+c = Counter(l)
+
+# Iterate through the Counter object to find elements with more than one occurrence
+for key, value in c.items():
+    if value > 1:
+        print(f"Element {key} occurs {value} times.")
+
 
 
